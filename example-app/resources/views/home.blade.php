e16cb1415f486b242ef5334696087b4b736b8842
CodeWithRibo <riboluna2818@gmail.com>
2025-04-02 20:25:09 +0800

    modify content

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <!-- This will be handle in main element -->
-    <h1>Welcome homepage</h1>
+    <h1 class="text-red-800">Welcome homepage</h1>
     <x-button></x-button>
 </x-layout>

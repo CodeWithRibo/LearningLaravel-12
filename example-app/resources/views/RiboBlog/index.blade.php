a0bce81baeef15247272c4f1d16052652a6d5ebb
CodeWithRibo <riboluna2818@gmail.com>
2025-04-07 19:45:28 +0800

    modify content

--- example-app/resources/views/RiboBlog/index.blade.php
+++ example-app/resources/views/RiboBlog/index.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Ribo Blog | Home</title>
-</head>
-<body>
-    <h1 class="text-2xl text-red-300">Welcome to Ribo Blog</h1>
-    <ul>
-        <li>List of Ribo Blogs</li>
-        <li>
-            <a href="{{$users['0']['id']}}" class="text-xl text-red-300 hover:text-red-500">{{$users['0']['name']}}</a>
-            <br>
-            <a href="{{$users['1']['id']}}" class="text-xl text-red-300 hover:text-red-500">{{$users['1']['name']}}</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <h1 class="pt-2">Currently Available Ribo Blogs</h1>
+    @foreach ($users as $user)
+            <x-card href="{{$user['id']}}" :highlight="$user['skill'] > 70">
+                <h1 class="text-2xl font-bold">{{$user['name']}}</h1>
+            </x-card>
+    @endforeach
+</x-layout>

a49287135dfe0efec5a8dc8b6dcd4eb830f0e747
CodeWithRibo <riboluna2818@gmail.com>
2025-04-07 19:46:58 +0800

    added create routes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,19 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 #Defined on what web browser reponsed in http request
 Route::get('/', function () {
-
     $data = [
-        'users' => [ 
-            ['name' => 'Alice', 'skill' => 'PHP', 'id' => 1],
-            ['name' => 'Charlie', 'skill' => 'Python', 'id' => 2],
-            ['name' => 'Charlie', 'skill' => 'Python', 'id' => 3],
-        ]
+        'users' => [
+        ['name' => 'Alice', 'skill' => '75', 'id' => 1], 
+        ['name' => 'Charlie', 'skill' => '70', 'id' => 2], 
+        ['name' => 'Bon', 'skill' => '80', 'id' => 3]],
     ];
 
-    return view('RiboBlog.index', ["users" => $data['users']]);
+    return view('RiboBlog.index', ['greeting' => 'Hello', 'users' => $data['users']]);
+});
+
+Route::get('create', function (){
+    return view('RiboBlog.create');
 });
 
 Route::get('/{id}', function ($id) {
-
-    return view('Riboblog.show', data: ["id" => $id]);
+    return view('Riboblog.show', data: ['id' => $id]);
 });
